5192c8708e3e9261bbb8d2b71feb87f5dfa46759
Vitor Andrioli <vitor@mandelbrot.com.br>
2016-10-12 10:19:13 -0300

    License and changes to gitignore.

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,10 @@
 class Api::UsersController < ApplicationController
   
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by_email(request.headers['X-User-Email'])
+    if @user.blank?
+      render json: {status: 400, message: "Could not find user"}.to_json
+    end 
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :sessions, only: [:create, :new]
     resources :registrations, only: :create
-    resources :users, only: :show
+    get 'user', to: 'users#show'
     
   end
   
